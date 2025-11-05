00469f28025eaad485bf3be7f8b0d88d528ec2ff
Nathan Louth <77612558+NathanLouth@users.noreply.github.com>
2025-11-05 12:51:02 +0000

    fix: overview not always hidden on login due to timing issue

--- skip-overview@nathanlouth.github.com/extension.js
+++ skip-overview@nathanlouth.github.com/extension.js
@@ -1,8 +1,12 @@
 import * as Main from 'resource:///org/gnome/shell/ui/main.js';
+import GLib from 'gi://GLib';
 
 export default class SkipOverviewExtension {
     enable() {
-        Main.overview.hide();
+        GLib.timeout_add(GLib.PRIORITY_DEFAULT, 100, () => {
+            Main.overview.hide();
+            return GLib.SOURCE_REMOVE;
+        });
     }
 
     disable() {
